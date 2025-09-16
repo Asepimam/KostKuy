733ef8a3abf41b419a299713eb835f3aced37c21
Asepimam <asepimammutaqin317@gmail.com>
2025-09-16 22:36:14 +0700

    Add RepositoryServiceProvider for binding repository interfaces to implementations

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Providers;
+
+use App\Interfaces\BaseRepositoryInterface;
+use App\Interfaces\BoardingHouseRepositoryInterface;
+use App\Interfaces\CategoryRepositoryInterface;
+use App\Interfaces\CityRepositoryInterface;
+use App\Interfaces\TransactionRepositoryInterface;
+use App\Repositories\BoardingHouseRepository;
+use App\Repositories\CategoryRepository;
+use App\Repositories\CityRepository;
+use App\Repositories\TransactionRepository;
+use Faker\Provider\Base;
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->app->bind(
+            BaseRepositoryInterface::class,
+        );
+        $this->app->bind(
+            CityRepositoryInterface::class, CityRepository::class
+        );
+        $this->app->bind(
+            CategoryRepositoryInterface::class, CategoryRepository::class
+        );
+        $this->app->bind(
+            BoardingHouseRepositoryInterface::class, BoardingHouseRepository::class
+        );
+
+        $this->app->bind(
+            TransactionRepositoryInterface::class, TransactionRepository::class
+        );
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
