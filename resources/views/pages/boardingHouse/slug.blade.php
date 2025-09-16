567a5687cb7a783fb3c0260e37fe616f07223ff1
Asepimam <asepimammutaqin317@gmail.com>
2025-09-17 03:03:47 +0700

    Refactor boarding house detail view with enhanced layout and dynamic content

--- resources/views/pages/boardingHouse/slug.blade.php
+++ resources/views/pages/boardingHouse/slug.blade.php
@@ -1,30 +1,172 @@
 @extends('layout.app')
 
 @section('content')
-<div class="w-full max-w-3xl mx-auto">
-    <!-- Thumbnail -->
-    <div class="relative w-full h-64 overflow-hidden rounded-b-2xl">
-        <img src="{{ Storage::url($boardingHouse->thumbnail) }}" alt="{{ $boardingHouse->name }}" class="w-full h-full object-cover">
-    </div>
-
-    <!-- Card yang mengambang -->
-    <div class="relative">
-        <div class="max-w-sm mx-auto -mt-16 p-6 bg-white border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    Noteworthy technology acquisitions 2021
-                </h5>
+        <div id="ForegroundFade" class="absolute top-0 w-full h-[143px] bg-[linear-gradient(180deg,#070707_0%,rgba(7,7,7,0)_100%)] z-10"></div>
+        <div id="TopNavAbsolute" class="absolute top-[60px] flex items-center justify-between w-full px-5 z-10">
+            <a href="index.html"
+                class="w-12 h-12 flex items-center justify-center shrink-0 rounded-full overflow-hidden bg-white/10 backdrop-blur-sm">
+                <img src="{{asset('assets/images/icons/arrow-left-transparent.svg')}}" class="w-8 h-8" alt="icon">
             </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-            </p>
-            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Read more
-                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                </svg>
-            </a>
+            <p class="font-semibold text-white">Details</p>
+            <button
+                class="w-12 h-12 flex items-center justify-center shrink-0 rounded-full overflow-hidden bg-white/10 backdrop-blur-sm">
+                <img src="{{asset('assets/images/icons/like.svg')}}" class="w-[26px] h-[26px]" alt="">
+            </button>
         </div>
-    </div>
-</div>
+       <div id="Gallery" class="swiper-gallery w-full overflow-x-hidden">
+            <div class="swiper-wrapper">
+                @foreach ($boardingHouse->rooms as $room)
+                    @foreach ($room->images as $imageroom)
+                        <div class="swiper-slide">
+                            <div class="w-full h-[430px] overflow-hidden">
+                                <img src="{{ Storage::url($imageroom->image) }}" 
+                                    class="w-full h-full object-cover"
+                                    alt="gallery thumbnails">
+                            </div>
+                        </div>
+                    @endforeach
+                @endforeach
+            </div>
+        </div>
+
+
+        <main id="Details" class="relative flex flex-col rounded-t-[40px] py-5 pb-[10px] gap-4 bg-white z-10">
+            <div id="Title" class="flex items-center justify-between gap-2 px-5">
+                <h1 class="font-bold text-[22px] leading-[33px]">{{$boardingHouse->name}}</h1>
+                <div
+                    class="flex flex-col items-center text-center shrink-0 rounded-[22px] border border-[#F1F2F6] p-[10px_20px] gap-2 bg-white">
+                    <img src="{{asset('assets/images/icons/star.svg')}}" class="w-6 h-6" alt="icon">
+                    <p class="font-bold text-sm">4/5</p>
+                </div>
+            </div>
+            <hr class="border-[#F1F2F6] mx-5">
+            <div id="Features" class="grid grid-cols-2 gap-x-[10px] gap-y-4 px-5">
+                <div class="flex items-center gap-[6px]">
+                    <img src="{{asset('assets/images/icons/location.svg')}}" class="w-[26px] h-[26px] flex shrink-0" alt="icon">
+                    <p class="text-ngekos-grey">{{ $boardingHouse->city->name }}</p>
+                </div>
+                <div class="flex items-center gap-[6px]">
+                    <img src="{{asset('assets/images/icons/3dcube.svg')}}" class="w-[26px] h-[26px] flex shrink-0" alt="icon">
+                    <p class="text-ngekos-grey">In {{ $boardingHouse->category->name }}</p>
+                </div>
+                <div class="flex items-center gap-[6px]">
+                    <img src="{{asset('assets/images/icons/profile-2user.svg')}}" class="w-[26px] h-[26px] flex shrink-0" alt="icon">
+                    <p class="text-ngekos-grey">{{ $boardingHouse->rooms->first()->capacity }} People</p>
+                </div>
+                <div class="flex items-center gap-[6px]">
+                    <img src="{{asset('assets/images/icons/shield-tick.svg')}}" class="w-[26px] h-[26px] flex shrink-0" alt="icon">
+                    <p class="text-ngekos-grey">Privacy 100%</p>
+                </div>
+            </div>
+            <hr class="border-[#F1F2F6] mx-5">
+            <div id="About" class="flex flex-col gap-[6px] px-5">
+                <h2 class="font-bold">About</h2>
+                <p class="leading-[30px]">{{strip_tags($boardingHouse->description)}}</p>
+            </div>
+            <div id="Tabs" class="swiper-tab w-full overflow-x-hidden">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide !w-fit">
+                        <button
+                            class="tab-link rounded-full p-[8px_14px] border border-[#F1F2F6] text-sm font-semibold hover:bg-ngekos-black hover:text-white transition-all duration-300 !bg-ngekos-black !text-white"
+                            data-target-tab="#Bonus-Tab">Bonus Kos</button>
+                    </div>
+                    <div class="swiper-slide !w-fit">
+                        <button
+                            class="tab-link rounded-full p-[8px_14px] border border-[#F1F2F6] text-sm font-semibold hover:bg-ngekos-black hover:text-white transition-all duration-300"
+                            data-target-tab="#Testimonials-Tab">Testimonials</button>
+                    </div>
+                    <div class="swiper-slide !w-fit">
+                        <button
+                            class="tab-link rounded-full p-[8px_14px] border border-[#F1F2F6] text-sm font-semibold hover:bg-ngekos-black hover:text-white transition-all duration-300"
+                            data-target-tab="#Rules-Tab">Rules</button>
+                    </div>
+                    <div class="swiper-slide !w-fit">
+                        <button
+                            class="tab-link rounded-full p-[8px_14px] border border-[#F1F2F6] text-sm font-semibold hover:bg-ngekos-black hover:text-white transition-all duration-300"
+                            data-target-tab="#Contact-Tab">Contact</button>
+                    </div>
+                    <div class="swiper-slide !w-fit">
+                        <button
+                            class="tab-link rounded-full p-[8px_14px] border border-[#F1F2F6] text-sm font-semibold hover:bg-ngekos-black hover:text-white transition-all duration-300"
+                            data-target-tab="#Rewards-Tab">Rewards</button>
+                    </div>
+                </div>
+            </div>
+            <div id="TabsContent" class="px-5">
+                <div id="Bonus-Tab" class="tab-content flex flex-col gap-5">
+                    <div class="flex flex-col gap-4">
+                        @foreach ($boardingHouse->bonuses as $bonus)
+                        <div
+                            class="bonus-card flex items-center rounded-[22px] border border-[#F1F2F6] p-[10px] gap-3 hover:border-[#91BF77] transition-all duration-300">
+                            <div class="flex w-[120px] h-[90px] shrink-0 rounded-[18px] bg-[#D9D9D9] overflow-hidden">
+                                <img src="{{ Storage::url($bonus->image) }}" class="w-full h-full object-cover"
+                                    alt="thumbnails">
+                            </div>
+                            <div>
+                                <p class="font-semibold">{{ $bonus->name }}</p>
+                                <p class="text-sm text-ngekos-grey">{{ $bonus->description }}</p>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
+                </div>
+                <div id="Testimonials-Tab" class="tab-content flex-col gap-5 hidden">
+                    <div class="flex flex-col gap-4">
+                        @foreach ($boardingHouse->testimonials as $testimonial)
+                            <div
+                                class="testi-card flex flex-col rounded-[22px] border border-[#F1F2F6] p-4 gap-3 bg-white hover:border-[#91BF77] transition-all duration-300">
+                                <div class="flex items-center gap-3">
+                                    <div
+                                        class="w-[70px] h-[70px] flex shrink-0 rounded-full border-4 border-white ring-1 ring-[#F1F2F6] overflow-hidden">
+                                        <img src="{{ Storage::url($testimonial->photo) }}" class="w-full h-full object-cover"
+                                            alt="icon">
+                                    </div>
+                                    <div>
+                                        <p class="font-semibold">{{ $testimonial->name }}</p>
+                                        <p class="mt-[2px] text-sm text-ngekos-grey">{{ $testimonial->created_at->format('d F Y') }}</p>
+                                    </div>
+                                </div>
+                                <p class="leading-[26px]">{{ $testimonial->content }}</p>
+                                <div class="flex">
+                                    @for ($i = 0; $i < $testimonial->rating; $i++)
+                                        <img src="{{ asset('assets/images/icons/Star 1.svg') }}" class="w-[22px] h-[22px] flex shrink-0"
+                                            alt="">
+                                    @endfor
+    
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+                <div id="Rules-Tab" class="tab-content flex-col gap-5 hidden">Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Porro, vitae.</div>
+                <div id="Contact-Tab" class="tab-content flex-col gap-5 hidden">Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Porro, vitae.</div>
+                <div id="Rewards-Tab" class="tab-content flex-col gap-5 hidden">Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Porro, vitae.</div>
+            </div>
+        </main>
+        <div id="BottomNav" class="relative flex w-full h-[138px] shrink-0">
+            <div class="fixed bottom-5 w-full max-w-[640px] px-5 z-10">
+                <div class="flex items-center justify-between rounded-[40px] py-4 px-6 bg-ngekos-black">
+                    <p class="font-bold text-xl leading-[30px] text-white">
+                        Rp 1.493.593
+                        <br>
+                        <span class="text-sm font-normal">/bulan</span>
+                    </p>
+                    @if ($boardingHouse->rooms->first()->is_available)
+                        <a href="room-available.html"
+                            class="flex shrink-0 rounded-full py-[14px] px-5 bg-ngekos-orange font-bold text-white">Book
+                            Now</a>        
+                    @else
+                        <a href="#"
+                            class="flex shrink-0 rounded-full py-[14px] px-5 bg-gray-400 font-bold text-white cursor-not-allowed">Not
+                            Available</a>    
+                    @endif
+                </div>
+            </div>
+        </div>
 @endsection
+@push('scripts')
+    <script src="{{ asset('assets/js/details.js') }}"></script>
+@endpush
