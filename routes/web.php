e6d280133ac01242d7e01bafa9bb3e55e1e841f0
Asepimam <asepimammutaqin317@gmail.com>
2025-09-17 03:04:37 +0700

    Update routing for boarding house search and detail booking

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', HomeController::class . '@index')->name('home');
 Route::get('/cities', CityController::class . '@index')->name('cities');
 Route::get('/cities/{city:slug}', CityController::class . '@slug')->name('city.slug');
+
 Route::get('/boarding-houses', BoardingHouseController::class . '@index')->name('boardingHouses');
 Route::get('/boarding-houses/{boardingHouse:slug}', BoardingHouseController::class . '@slug')->name('boardingHouse.slug');
-Route::get('/find-boarding-houses', BoardingHouseController::class . '@findBoardingHouses')->name('find.boardingHouses');
+Route::get('/find-boarding-house-search', BoardingHouseController::class . '@findBoardingHouses')->name('find.boardingHouses.search');
+Route::get('/find-boarding-house', BoardingHouseController::class . '@find')->name('find.boardingHouse');
+
 Route::get('/check-booking', BookingController::class . '@checkBooking')->name('check.booking');
+Route::get('/detail-booking', BookingController::class . '@detailBooking')->name('detail.booking');
